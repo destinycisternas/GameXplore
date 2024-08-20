7e7125531f381922c7020844113bcc6b6be9940c
DestinyCisternas <destinycisternas@gmail.com>
2024-08-19 23:02:50 -0400

    Fix Menu

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -9,7 +9,6 @@ const Header = () => {
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#games">Games</a></li>
-                    <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                     <li><a href="#login" className="login-wrapper"><span className="login-button">Log In</span></a></li>
                 </ul>
